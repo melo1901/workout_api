2963cdd31053de62c42c546ae213d7fa3233caff
Melo <meluszko@gmail.com>
2023-08-25 22:46:43 +0200

    basic sqlalchemy testing

---
+++ api/main.py
@@ -0,0 +1,21 @@
+from sqlalchemy import create_engine
+from sqlalchemy.exc import SQLAlchemyError
+from sqlalchemy.orm import sessionmaker
+from models.user import User
+from models.base import Base
+
+db_url = "mysql+mysqlconnector://root:password@localhost:3306/api"
+
+try:
+    engine = create_engine(db_url, echo=True)
+    Base.metadata.create_all(engine)
+    
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    
+    user = User(nickname="test", name="Test", surname="Test", email="test", join_date="2018-01-01")
+    session.add(user)
+    session.commit()
+    
+except SQLAlchemyError as e:
+    print("Connection failed: ", e)
---
+++ api/models/user.py
@@ -0,0 +1,23 @@
+from sqlalchemy import Column, String, Date
+from models.base import Base
+
+class User(Base):
+    __tablename__ = "user"
+    
+    nickname = Column(String(50), unique=True, primary_key=True)
+    name = Column(String(50))
+    surname = Column(String(50))
+    email = Column(String(50), unique=True)
+    join_date = Column(Date)
+    
+    def __init__(self, nickname, name, surname, email, join_date):
+        self.nickname = nickname
+        self.name = name
+        self.surname = surname
+        self.email = email
+        self.join_date = join_date
+        
+    def __repr__(self):
+        return "<User(nickname='%s', name='%s', surname='%s', email='%s', join_date='%s')>" % (
+            self.nickname, self.name, self.surname, self.email, self.join_date)
+
